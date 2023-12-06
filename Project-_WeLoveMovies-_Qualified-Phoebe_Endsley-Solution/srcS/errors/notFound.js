734ed7b90f69c1bf323b9af6e5514f1910dc3c4f
Phabolous <138738469+Phabolous@users.noreply.github.com>
2023-12-06 14:43:35 +0000

    Delete Project-_WeLoveMovies-_Qualified-Phoebe_Endsley-Solution directory

--- Project-_WeLoveMovies-_Qualified-Phoebe_Endsley-Solution/srcS/errors/notFound.js
+++
@@ -1,5 +0,0 @@
-function notFound(req, res, next) {
-  next({ staus: 404, message: `Not found: ${req.originalUrl}` });
-}
-
-module.exports = notFound;
--- Project-_WeLoveMovies-_Qualified-Phoebe_Endsley-Solution/srcS/theaters/theaters.router.js
+++
@@ -1,7 +0,0 @@
-const router = require("express").Router();
-const methodNotAllowed = require("../errors/methodNotAllowed");
-const controller = require("./theaters.controller");
-
-router.route("/").get(controller.list).all(methodNotAllowed);
-
-module.exports = router;
